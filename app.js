99aa56dac4199cd8f1728feb22d552835c6e1e3f
Scott Laughton <codeowl@gmail.com>
2018-01-02 12:52:32 +1100

    no message

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-var http = require('http');
+var http = require('http'),
+    app = require('./app');
 
-var port = 3002,
-    host = 'regio.fr.openode.io';
+//var port = '80',
+//    host = 'regio.fr.openode.io';
     //host = 'localhost';
 
-http.createServer(function (req, res) {
+var port = normalizePort(process.env.PORT || '3002');
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+server.on('listening', onListening);
+server.on('request', function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 
-}).listen(port, host);
+});
 
-console.log('Server running at ' + host + ':' + port);
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    console.log('Listening on ' + bind);
+}
 
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+    return false;
+}
